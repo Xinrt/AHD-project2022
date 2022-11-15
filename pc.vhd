373e6a94751c14e445f29ef7b4161b12d4e0d24f
Qing "Allen" Xiang <93733254+allenius-xiang@users.noreply.github.com>
2022-11-15 16:39:04 -0500

    Add comments to pc.vhd

--- pc.vhd
+++ pc.vhd
@@ -2,17 +2,18 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity pc is
-    Port (clk: in std_logic;
-          rst: in std_logic;
-          addr_in: in std_logic_vector (31 downto 0);
-          addr_out: out std_logic_vector (31 downto 0));
+    Port (clk: in std_logic;    --clock signal
+          rst: in std_logic;    --asynchronous reset signal
+          addr_in: in std_logic_vector (31 downto 0);   --32-bit imem address input
+          addr_out: out std_logic_vector (31 downto 0)  --32-bit imem address output
+         );
 end pc;
 
 architecture Behavioral of pc is
 signal addr: std_logic_vector(31 downto 0);
 begin
 process(clk,rst) begin
-    if rst = '1' then addr <= x"01000000";
+    if rst = '1' then addr <= x"01000000";  --imem address resets to 0x01000000
     elsif clk = '1' then addr <= addr_in;
     end if;
 end process;
