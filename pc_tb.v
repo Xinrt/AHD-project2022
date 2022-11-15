42a94ccbcdd13074f28d81cd7351661ea5c4b88b
Qing "Allen" Xiang <93733254+allenius-xiang@users.noreply.github.com>
2022-11-15 16:46:33 -0500

    Change timescale

--- pc_tb.v
+++ pc_tb.v
@@ -1,4 +1,4 @@
-`timescale 1ps / 1ps
+`timescale 1ns / 1ps
 module pc_tb();
 reg t_clk, t_rst;
 reg [31:0]t_addr_in;
@@ -9,31 +9,31 @@ pc dut_pc(
     .addr_in(t_addr_in),
     .addr_out(t_in_out)
 );
-always #1 t_clk = ~t_clk;
+always #5 t_clk = ~t_clk;
 
 initial begin
     t_addr_in = 32'h0;
     t_clk = 0;
     t_rst = 1;
-    #2
+    #10
     if (t_addr_out != 32'h01000000) begin   //test asynchronous reset
         $display("Incorrect pc reset behavior.");
         $stop;
     end
     t_rst = 0;
-    #2
+    #10
     if (t_addr_out != 32'h0) begin          //test address 0
         $display("Incorrect pc output.");
         $stop;
     end
     t_addr_in = 32'hff;
-    #3
+    #10
     if (t_addr_out != 32'hff) begin         //test random address
         $display("Incorrect pc output.");
         $stop;
     end
     t_rst = 1;
-    #1
+    #5
     if (t_addr_out != 32'h01000000) begin   //test reset again
         $display("Incorrect pc reset behavior.");
         $stop;
