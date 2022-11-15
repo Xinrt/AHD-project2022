82ab2449e22a806a33ca5a01bf334ea4d917b4df
Qing "Allen" Xiang <93733254+allenius-xiang@users.noreply.github.com>
2022-11-15 16:45:20 -0500

    Add immediate generator and its testbenches

---
+++ imm_gen.vhd
@@ -0,0 +1,43 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity imm_gen is
+  Port (instr: in std_logic_vector (31 downto 0);   --32-bit instruction
+        imm_out: out std_logic_vector (31 downto 0) --32-bit signed immediate value
+        );
+end imm_gen;
+
+architecture Behavioral of imm_gen is
+constant pos: std_logic_vector (19 downto 0):= x"00000";
+constant neg: std_logic_vector (19 downto 0):= x"fffff";
+signal imm: std_logic_vector (31 downto 0);
+
+begin
+process(instr) begin
+    if instr(6 downto 0) = b"1100111" or instr(6 downto 0)  = b"0000011" then   --I-type
+        if instr(31) = '0' then imm <= pos & instr(31 downto 20);
+        else imm <= neg & instr(31 downto 20);
+        end if;
+    elsif instr(6 downto 0) = b"0100011" then   --S-type
+        if instr(31) = '0' then imm <= pos & instr(31 downto 25)&instr(11 downto 7);
+        else imm <= neg & instr(31 downto 25)&instr(11 downto 7);
+        end if;
+    elsif instr(6 downto 0) = b"1100011" then   --B-type
+        if instr(31) = '0' then imm <= pos(18 downto 0) &
+            instr(31)&instr(7)&instr(30 downto 25)&instr(11 downto 8) & '0';
+        else imm <= neg(18 downto 0) &
+            instr(31)&instr(7)&instr(30 downto 25)&instr(11 downto 8) & '0';
+        end if;
+    elsif instr(6 downto 0) = b"0110111" or instr(6 downto 0) = b"0010111" then --U-type
+        imm <= instr(31 downto 12) & pos(11 downto 0);
+    elsif instr(6 downto 0) = b"1101111" then   --J-type
+        if instr(31) = '0' then imm <= pos(10 downto 0) &
+            instr(31)&instr(19 downto 12)&instr(20)&instr(30 downto 21) & '0';
+        else imm <= neg(10 downto 0) &
+            instr(31)&instr(19 downto 12)&instr(20)&instr(30 downto 21) & '0';
+        end if;
+    else imm <= x"00000000";    --nop
+    end if;
+end process;
+imm_out <= imm;
+end Behavioral;
---
+++ imm_gen_tb.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+module imm_gen_tb();
+reg t_clk;
+reg [31:0]t_instr;
+wire [31:0]t_imm_out;
+
+imm_gen dut_imm_gen(
+    .instr(t_instr),
+    .imm_out(t_imm_out)
+);
+
+always #5 t_clk = ~t_clk;
+
+initial begin
+    t_clk = 0;
+    t_instr = 32'h0;    //nop
+    #10
+    if (t_imm_out != 32'h0) begin
+        $display("Incorrect default output.");
+        $stop;
+    end
+    t_instr = 32'h6cdff6e7; //I-type positive
+    #10
+    if (t_imm_out != 32'h000006cd) begin
+        $display("Incorrect I-type sign extension for postive value.");
+        $display(t_imm_out);
+        $stop;
+    end
+    t_instr = 32'h8cdff603; //I-type negative
+    #10
+    if (t_imm_out != 32'hfffff8cd) begin
+        $display("Incorrect I-type sign extension for negative value.");
+        $stop;
+    end
+    t_instr = 32'h02dff6a3; //S-type positive
+    #10
+    if (t_imm_out != 32'h0000002d) begin
+        $display("Incorrect S-type sign extension for positive value.");
+        $stop;
+    end
+    t_instr = 32'ha2dff623; //S-type negative
+    #10
+    if (t_imm_out != 32'hfffffa2c) begin
+        $display("Incorrect S-type sign extension for negative value.");
+        $stop;
+    end
+    t_instr = 32'h75834863; //B-type positive
+    #10
+    if (t_imm_out != 32'h00000750) begin
+        $display("Incorrect B-type sign extension for positive value.");
+        $stop;
+    end
+    t_instr = 32'hf1eed7e3; //B-type negative
+    #10
+    if (t_imm_out != 32'hffffff0e) begin
+        $display("Incorrect B-type sign extension for negative value.");
+        $stop;
+    end
+    t_instr = 32'h12345637; //U-type positive
+    #10
+    if (t_imm_out != 32'h12345000) begin
+        $display("Incorrect U-type sign extension for positive value.");
+        $stop;
+    end
+    t_instr = 32'habcdef97; //U-type negative
+    #10
+    if (t_imm_out != 32'habcde000) begin
+        $display("Incorrect U-type sign extension for negative value.");
+        $stop;
+    end
+    t_instr = 32'h6669996f; //J-type positive
+    #10
+    if (t_imm_out != 32'h00099666) begin
+        $display("Incorrect J-type sign extension for positive value.");
+        $stop;
+    end
+    t_instr = 32'h888888ef; //J-type negative
+    #10
+    if (t_imm_out != 32'hfff88088) begin
+        $display("Incorrect J-type sign extension for negative value.");
+        $stop;
+    end   
+    $display("All tests passed.");
+    $stop;
+end
+endmodule
