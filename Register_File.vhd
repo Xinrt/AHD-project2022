6640fa4b33726f353e1502113b91e06791035908
TsunamiDing <68684342+TsunamiDing@users.noreply.github.com>
2022-11-17 19:16:44 -0500

    Update Register_File.vhd

--- Register_File.vhd
+++ Register_File.vhd
@@ -37,6 +37,9 @@ entity Register_File is
     Port (clk : in STD_LOGIC;
           rst : in STD_LOGIC;
           
+          -- control signal
+          read_or_write : in STD_LOGIC_VECTOR (1 DOWNTO 0);
+          
           -- register id that will be read or write
           read_addr_1 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
           read_addr_2 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
@@ -74,22 +77,28 @@ architecture Behavioral of Register_File is
     
 begin
     
-    -- write data into write_addr of register file
     process(clk, rst) begin
         if (rst = '1') then
             registers_32 <= registers_32_rst;
         elsif (clk'event and clk = '1') then
-            if (write_addr /= "00000") then -- R0 should always be 0 and cannot be written in other data
-                registers_32(CONV_INTEGER(write_addr)) <= write_data;
+            
+              -- write data into write_addr of register file
+            if (read_or_write(0) = '1') then
+                if (write_addr /= "00000") then -- R0 should always be 0 and cannot be written in other data
+                    registers_32(CONV_INTEGER(write_addr)) <= write_data;
+                end if;
             end if;
+            
+            -- read data from read_addr of register file
+            if (read_or_write(1) = '1') then
+                read_data_1 <= registers_32(CONV_INTEGER(read_addr_1));
+                read_data_2 <= registers_32(CONV_INTEGER(read_addr_2));
+            end if;
+            
         end if;
     end process;
     
     -- R0 of registers is special that it is read-only and is always zero
     registers_32(0) <= "00000000000000000000000000000000";
-    
-    -- write data from read_addr of register file
-    read_data_1 <= registers_32(CONV_INTEGER(read_addr_1));
-    read_data_2 <= registers_32(CONV_INTEGER(read_addr_2));
     
 end Behavioral;
