be4551fb91d4c4cf9c66b2765195dbe5b399a915
TsunamiDing <68684342+TsunamiDing@users.noreply.github.com>
2022-11-17 23:49:10 -0500

    Update Register_File_tb.v

--- Register_File_tb.v
+++ Register_File_tb.v
@@ -25,6 +25,8 @@ module Register_File_tb();
     reg t_clk;
     reg t_rst;
     
+    reg [1:0] t_read_or_write;
+    
     reg [4:0] t_read_addr_1;
     reg [4:0] t_read_addr_2;
     reg [4:0] t_write_addr;
@@ -38,6 +40,7 @@ module Register_File_tb();
     Register_File Register_File_tb(
         .clk(t_clk),
         .rst(t_rst),
+        .read_or_write(t_read_or_write),
         .read_addr_1(t_read_addr_1),
         .read_addr_2(t_read_addr_2),
         .write_addr(t_write_addr),
@@ -52,67 +55,109 @@ module Register_File_tb();
     end
     
     initial begin
-        t_clk = 0;
+        t_clk = 1;
         t_rst = 1;
+        t_read_or_write = 2'b00;
         #5
         
-        // test initial reset
-        for (i = 0; i < 32; i = i + 1) begin
-            t_read_addr_1 = i;
-            t_read_addr_2 = i;
-            #5
-            if (t_read_data_1 != 32'b0 | t_read_data_2 != 32'b0) begin
-                $display("Incorrect initial reset");
-                $stop;
-            end
-            #5;
-        end
-        
+        // remove reset and enable whole RF
         t_rst = 0;
+        
         // write f0f0f0f0 to R1 and read it to first output
         t_write_addr = 5'b00001;
         t_write_data = 32'b11110000111100001111000011110000;
         t_read_addr_1 = 5'b00001;
-        #5
-        if (t_read_data_1 != 32'b11110000111100001111000011110000) begin
-            $display("Incorrect write and read");
-            $stop;
-        end
-        #5;
-        
-        // write 0f0f0f0f to R31 and read it to second output
-        t_write_addr = 5'b11111;
-        t_write_data = 32'b00001111000011110000111100001111;
-        t_read_addr_2 = 5'b11111;
-        #5
-        if (t_read_data_2 != 32'b00001111000011110000111100001111) begin
-            $display("Incorrect write and read");
-            $stop;
-        end
-        #5;
-        
-        // write f0f0f0f0 to R0 (it should fail because R0 is always 0) and read it to first output
-        t_write_addr = 5'b00000;
-        t_write_data = 32'b11110000111100001111000011110000;
-        t_read_addr_1 = 5'b00000;
-        #5
+        #10
+        // cannot read or write, so output should be 0
         if (t_read_data_1 != 32'b0) begin
             $display("Incorrect write and read");
             $stop;
         end
-        #5;
+        // read only and cannot write
+        t_read_or_write = 2'b10;
+        #10;
+        if (t_read_data_1 != 32'b0) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        // write enable
+        t_read_or_write = 2'b01;        
+        #10;        
+        if (t_read_data_1 != 32'b0) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        // read enable, and output will be same as input
+        t_read_or_write = 2'b10;
+        #10; 
+        if (t_read_data_1 != 32'b11110000111100001111000011110000) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        #10;        
+
+        // write 0f0f0f0f to R31 and read it to second output
+        t_write_addr = 5'b11111;
+        t_write_data = 32'b00001111000011110000111100001111;
+        t_read_addr_2 = 5'b11111;
+        #10
+        // cannot read or write, so output should be 0
+        if (t_read_data_2 != 32'b0) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        // read only and cannot write
+        t_read_or_write = 2'b10;
+        #10;
+        if (t_read_data_2 != 32'b0) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        // write enable
+        t_read_or_write = 2'b01;        
+        #10;        
+        if (t_read_data_2 != 32'b0) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        // read enable, and output will be same as input
+        t_read_or_write = 2'b10;
+        #10; 
+        if (t_read_data_2 != 32'b00001111000011110000111100001111) begin
+            $display("Incorrect write and read");
+            $stop;
+        end
+        #10;
         
+        // write 0f0f0f0f to R0 and read it to second output. R0 should always be 0 and cannot be write in other data.
+        t_write_addr = 5'b00000;
+        t_write_data = 32'b00001111000011110000111100001111;
+        t_read_addr_2 = 5'b00000;
+        t_read_or_write = 2'b11;
+        #10;
+        // 
+        if (t_read_data_2 != 32'b0) begin
+            $display("Incorrect, R0 has been written in");
+            $stop;
+        end
+        #10;
+
         // reset all registers and test
         t_rst = 1;
+        #10;
+        // stop reset (otherwise read_data will always be 0)
+        t_rst = 0;
+        #10;
+        // enable read
+        t_read_or_write = 2'b10;
         for (i = 0; i < 32; i = i + 1) begin
             t_read_addr_1 = i;
             t_read_addr_2 = i;
-            #5
+            #10
             if (t_read_data_1 != 32'b0 | t_read_data_2 != 32'b0) begin
                 $display("Incorrect reset");
                 $stop;
             end
-            #5;
         end       
         
         $display("all tests passed");
