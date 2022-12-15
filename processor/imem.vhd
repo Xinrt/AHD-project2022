98e7aee90fb8e22a60eb07e51970de7db24f42ff
Qing "Allen" Xiang <93733254+allenius-xiang@users.noreply.github.com>
2022-12-15 00:24:36 -0500

    Update imem.vhd

--- processor/imem.vhd
+++ processor/imem.vhd
@@ -2,6 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL; 
 use IEEE.std_logic_unsigned.all;
 use IEEE.NUMERIC_STD.ALL;
+use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
 
 entity imem is
   Port ( 
@@ -21,15 +24,33 @@ architecture Behavioral of imem is
 -- ROM_LENGTH_WORDS = 512
 
 type instr_rom is array(0 to 512-1) of std_logic_vector(31 downto 0);
+-- Read a *.hex file
+impure function instr_rom_readfile(FileName : STRING) return
+instr_rom is
+file FileHandle : TEXT open READ_MODE is FileName;
+variable CurrentLine : LINE;
+variable CurrentWord : std_logic_vector(31 downto 0);
+variable Result : instr_rom := (others => (others => 'X'));
+begin
+for i in 0 to 512 - 1 loop
+exit when endfile(FileHandle);
+readline(FileHandle, CurrentLine);
+hread(CurrentLine, CurrentWord);
+Result(i) := CurrentWord;
+end loop;
+return Result;
+end function;
+
 --signal rom_words: instr_rom;        -- word addressed read only memory
 signal addr_word: std_logic_vector(31 downto 0) := x"00000000";   -- 32-bit word addressed pc address input
 
 -- store addi addi add j
--- rom_words(0£©= x"00100093"  the instruction at the 1st address is x"00100093" (addi x1, x0, 1)
--- rom_words(1£©= x"00200113" the instruction at the 2nd address is x"00200113" (addi x2, x0, 2)
--- rom_words(2£©= x"002080b3" the instruction at the 3rd address is x"002080b3" (add x1, x1, x2)
--- rom_words(3£©= x"ffdff06f" the instruction at the 4th address is x"ffdff06f" (j loop)
-signal rom_words: instr_rom := (x"00100093", x"00200113", x"002080b3", x"ffdff06f", others => (others =>'X'));
+-- rom_words(0Â£Â©= x"00100093"  the instruction at the 1st address is x"00100093" (addi x1, x0, 1)
+-- rom_words(1Â£Â©= x"00200113" the instruction at the 2nd address is x"00200113" (addi x2, x0, 2)
+-- rom_words(2Â£Â©= x"002080b3" the instruction at the 3rd address is x"002080b3" (add x1, x1, x2)
+-- rom_words(3Â£Â©= x"ffdff06f" the instruction at the 4th address is x"ffdff06f" (j loop)
+--signal rom_words: instr_rom := (x"00100093", x"00200113", x"002080b3", x"ffdff06f", others => (others =>'X'));
+signal rom_words: instr_rom := instr_rom_readfile("main.mem");
 signal pc0: std_logic_vector(31 downto 0) := x"01000000";
 begin
 -- ROM_LENGTH_BITS = 2024 = 2^11 
@@ -43,13 +64,19 @@ begin
 addr_word(11-3 downto 0) <= addr(11-1 downto 2);
 
 process(clk, rst) begin
+--report "The value of 'rom0' is " & integer'image(to_integer(unsigned(rom_words(0))));
+--report "The value of 'rom1' is " & integer'image(to_integer(unsigned(rom_words(1))));
+--report "The value of 'rom2' is " & integer'image(to_integer(unsigned(rom_words(2))));
+--report "The value of 'rom3' is " & integer'image(to_integer(unsigned(rom_words(3))));
+
     if (imemR = '1') then
         if (rst = '1') then       -- PC resets to 0x01000000 -> addr = 0x01000000
             -- to_integer(unsigned(addr_word)) = 0
-            instr <= rom_words(0); 
+            rom_words <=  (x"00000000", others => (others =>'0'));  -- clear the array
+            instr <= x"00000000"; 
         elsif (clk'event and clk = '1') then 
             -- to_integer(unsigned(addr_word)) = 0, 1, 2, 3
-            instr <= rom_words(to_integer(unsigned(addr_word))+1);
+            instr <= rom_words(to_integer(unsigned(addr_word)));
         end if;
     else
         instr <= x"00000000";
