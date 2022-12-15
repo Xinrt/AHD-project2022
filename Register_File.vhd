3b626597915c65cf581b6670bbd52a2c55fea988
TsunamiDing <68684342+TsunamiDing@users.noreply.github.com>
2022-12-15 00:57:51 -0500

    Update Register_File.vhd

--- Register_File.vhd
+++ Register_File.vhd
@@ -20,9 +20,19 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+package reg_32_pkg is
+    type reg_32 is array(31 DOWNTO 0) of STD_LOGIC_VECTOR(31 DOWNTO 0);
+end package;
+
+library IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE work.reg_32_pkg.all;
 
 entity rf is
     Port (clk : in STD_LOGIC;
@@ -42,14 +52,14 @@ entity rf is
           dout2 : out STD_LOGIC_VECTOR (31 DOWNTO 0) := "00000000000000000000000000000000";
           
           -- the output register file, including 32 registers
-          registers_out : out register_array
+          reg_out : out reg_32
     );
 end rf;
 
 architecture Behavioral of rf is
     
     type register_array is ARRAY (31 DOWNTO 0) of STD_LOGIC_VECTOR(31 DOWNTO 0);
-    constant registers_32_rst : register_array := (
+    constant registers_32_rst : reg_32 := (
     "00000000000000000000000000000000", "00000000000000000000000000000000",
     "00000000000000000000000000000000", "00000000000000000000000000000000",
     "00000000000000000000000000000000", "00000000000000000000000000000000",
@@ -67,7 +77,7 @@ architecture Behavioral of rf is
     "00000000000000000000000000000000", "00000000000000000000000000000000",
     "00000000000000000000000000000000", "00000000000000000000000000000000"
     );
-    signal registers_32 : register_array := registers_32_rst;
+    signal registers_32 : reg_32 := registers_32_rst;
     
 begin
     
@@ -103,6 +113,6 @@ begin
             
         end if;
     end process;
-    registers_out <= registers_32;
+    reg_out <= registers_32;
     
 end Behavioral;
