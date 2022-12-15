b51f30f5ce4e24ea4d71281540cff60cb4fd2c31
Qing "Allen" Xiang <93733254+allenius-xiang@users.noreply.github.com>
2022-12-15 00:40:42 -0500

    Update pc.vhd

--- processor/pc.vhd
+++ processor/pc.vhd
@@ -4,6 +4,7 @@ use IEEE.std_logic_1164.ALL;
 entity pc is
     Port (clk: in std_logic;    --clock signal
           rst: in std_logic;    --asynchronous reset signal
+          en: in std_logic;     --global enable signal
           pcW: in std_logic;    --write pc control signal
           addr_in: in std_logic_vector (31 downto 0);   --32-bit imem address input
           addr_out: out std_logic_vector (31 downto 0)  --32-bit imem address output
@@ -11,13 +12,15 @@ entity pc is
 end pc;
 
 architecture Behavioral of pc is
-signal addr: std_logic_vector(31 downto 0);
+signal addr: std_logic_vector(31 downto 0):= x"01000000";
 begin
 process(clk,rst) begin
     if rst = '1' then addr <= x"01000000";  --address resets to 0x01000000
-    elsif clk'event and clk = '1' then 
-        if pcW = '1' then addr <= addr_in;  --update address
-    end if;
+    elsif en = '1' then
+        if clk'event and clk = '1' then 
+            if pcW = '1' then addr <= addr_in;  --update address
+            end if;
+        end if;
     end if;
 end process;
 addr_out <= addr;
