7bc278465a40a299bbead2de0469a6cc3e2887eb
TsunamiDing <68684342+TsunamiDing@users.noreply.github.com>
2022-12-15 00:21:06 -0500

    Update Register_File.vhd

--- Register_File.vhd
+++ Register_File.vhd
@@ -24,35 +24,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity Register_File is
+entity rf is
     Port (clk : in STD_LOGIC;
           rst : in STD_LOGIC;
           
           -- control signal
-          read_or_write : in STD_LOGIC_VECTOR (1 DOWNTO 0);
+          regRW : in STD_LOGIC_VECTOR (1 DOWNTO 0);
           
           -- register id that will be read or write
-          read_addr_1 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
-          read_addr_2 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
-          write_addr  : in STD_LOGIC_VECTOR (4 DOWNTO 0);
+          r1 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
+          r2 : in STD_LOGIC_VECTOR (4 DOWNTO 0);
+          rd  : in STD_LOGIC_VECTOR (4 DOWNTO 0);
           
           -- the data that will be written in registers or read out from registers
-          write_data  : in STD_LOGIC_VECTOR (31 DOWNTO 0);
-          read_data_1 : out STD_LOGIC_VECTOR (31 DOWNTO 0) := "00000000000000000000000000000000";
-          read_data_2 : out STD_LOGIC_VECTOR (31 DOWNTO 0) := "00000000000000000000000000000000"
+          din  : in STD_LOGIC_VECTOR (31 DOWNTO 0);
+          dout1 : out STD_LOGIC_VECTOR (31 DOWNTO 0) := "00000000000000000000000000000000";
+          dout2 : out STD_LOGIC_VECTOR (31 DOWNTO 0) := "00000000000000000000000000000000";
+          
+          -- the output register file, including 32 registers
+          registers_out : out register_array
     );
-end Register_File;
+end rf;
 
-architecture Behavioral of Register_File is
+architecture Behavioral of rf is
     
     type register_array is ARRAY (31 DOWNTO 0) of STD_LOGIC_VECTOR(31 DOWNTO 0);
     constant registers_32_rst : register_array := (
@@ -82,32 +76,33 @@ begin
         -- reset
         if (rst = '1') then
             registers_32 <= registers_32_rst;
-            read_data_1 <= "00000000000000000000000000000000";
-            read_data_2 <= "00000000000000000000000000000000";
+            dout1 <= "00000000000000000000000000000000";
+            dout2 <= "00000000000000000000000000000000";
                 
         elsif (clk'event and clk = '1') then
              -- can only write data into write_addr of register file
-            if (read_or_write = "10") then
-                if (write_addr /= "00000") then -- R0 should always be 0 and cannot be written in other data
-                    registers_32(CONV_INTEGER(write_addr)) <= write_data;
+            if (regRW = "10") then
+                if (rd /= "00000") then -- R0 should always be 0 and cannot be written in other data
+                    registers_32(CONV_INTEGER(rd)) <= din;
                 else
                     registers_32(0) <= "00000000000000000000000000000000"; -- R0 is always zero
                 end if;
-                read_data_1 <= "00000000000000000000000000000000";
-                read_data_2 <= "00000000000000000000000000000000";
+                dout1 <= "00000000000000000000000000000000";
+                dout2 <= "00000000000000000000000000000000";
                             
             -- can only read data from address read_addr of register file
-            elsif (read_or_write = "01") then
-                read_data_1 <= registers_32(CONV_INTEGER(read_addr_1));
-                read_data_2 <= registers_32(CONV_INTEGER(read_addr_2));
+            elsif (regRW = "01") then
+                dout1 <= registers_32(CONV_INTEGER(r1));
+                dout2 <= registers_32(CONV_INTEGER(r2));
             
             -- cannot read and cannot write
             else 
-                read_data_1 <= "00000000000000000000000000000000";
-                read_data_2 <= "00000000000000000000000000000000";
+                dout1 <= "00000000000000000000000000000000";
+                dout2 <= "00000000000000000000000000000000";
             end if;
             
         end if;
     end process;
+    registers_out <= registers_32;
     
 end Behavioral;
